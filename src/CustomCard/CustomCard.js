e00635c4551ed14bee8217ad3f194bfe3d6cc013
rahulreddyynr <51309218+rahulreddyynr@users.noreply.github.com>
2022-11-21 22:11:53 +0530

    2 files added

---
+++ src/CustomCard/CustomCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './CustomCard.css';
+
+export default function Card() {
+  return (
+    <div>
+      <span>{title}</span>
+      <span>{auther}</span>
+    </div>
+  );
+}
