45a5baf0056fb39989d10a5a3aa4e0f0a0843b5b
rahulreddyynr <51309218+rahulreddyynr@users.noreply.github.com>
2022-11-21 22:15:25 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './style.css';
+import CustomCard from './CustomCard/CustomCard';
 
 export default function App() {
   const Data = [
-    { author: 'rahul', title: 'left top' },
-    { author: 'rahul1', title: 'left top' },
-    { author: 'rahul2', title: 'left top' },
-    { author: 'rahul3', title: 'left top' },
-    { author: 'rahul4', title: 'left top' },
+    { author: 'rahul', title: 'left top', Desi: 'some dummy test' },
+    { author: 'rahul1', title: 'left top', Desi: 'some dummy test' },
+    { author: 'rahul2', title: 'left top', Desi: 'some dummy test' },
+    { author: 'rahul3', title: 'left top', Desi: 'some dummy test' },
+    { author: 'rahul4', title: 'left top', Desi: 'some dummy test' },
   ];
 
   return (
