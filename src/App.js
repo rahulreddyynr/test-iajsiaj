b0d6b9b291c3983c5cd430cda245a8ec140978c5
rahulreddyynr <51309218+rahulreddyynr@users.noreply.github.com>
2022-11-21 22:09:22 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
+  const Data = [
+    { author: 'rahul', title: 'left top' },
+    { author: 'rahul1', title: 'left top' },
+    { author: 'rahul2', title: 'left top' },
+    { author: 'rahul3', title: 'left top' },
+    { author: 'rahul4', title: 'left top' },
+  ];
+
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
